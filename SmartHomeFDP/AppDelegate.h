b85e233c5167912b05d84a4337c4fcf32976f4c4
张希报 <13210240126@fudan.edu.cn>
2014-10-30 13:52:50 +0800

    Initial Commit

---
+++ SmartHomeFDP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartHomeFDP
+//
+//  Created by eddie on 14-10-30.
+//  Copyright (c) 2014年 eddie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartHomeFDP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartHomeFDP
+//
+//  Created by eddie on 14-10-30.
+//  Copyright (c) 2014年 eddie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
